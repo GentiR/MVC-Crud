548b6bd06a1912ae535ae4f3506282c1048ee3a2
Gent Rechica <gr48119@ubt-uni.net>
2021-12-14 02:29:23 +0100

    Add project files.

---
+++ MvcCrud/Models/EmployeeContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCrud.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext()
+        {
+
+        }
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
